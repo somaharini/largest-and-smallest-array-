66ccfd505826ef95006ae71de5e31cfd617eb999
somaharini <somaharini24@gmail.com>
2025-01-30 22:21:58 +0530

    Create Largestandsmallest.java

---
+++ Largestandsmallest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LargestAndSmallest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+                int largest = array[0];
+        int smallest = array[0];
+                for (int i = 1; i < n; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+            }
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        System.out.println("Largest element: " + largest);
+        System.out.println("Smallest element: " + smallest);
+        scanner.close();
+    }
+}
